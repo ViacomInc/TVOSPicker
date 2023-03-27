3684d3f431d7ee920b648a8f00aea3692fd98ca9
Pawel Wszeborowski <pawel.wszeborowski@paramount.com>
2023-03-27 12:45:02 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TVOSPickerView
+
+A description of this package.
---
+++ Tests/TVOSPickerViewTests/TVOSPickerViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TVOSPickerView
+
+final class TVOSPickerViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TVOSPickerView().text, "Hello, World!")
+    }
+}
